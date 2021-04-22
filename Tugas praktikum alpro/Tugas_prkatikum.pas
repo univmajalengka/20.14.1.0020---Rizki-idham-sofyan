0c11b3b1b81c303109ece384528d5865c5c1d345
Rizki-132 <77156567+Rizki-132@users.noreply.github.com>
2021-04-22 16:51:34 +0700

    Add files via upload

---
+++ Tugas praktikum alpro/Tugas_prkatikum.pas
@@ -0,0 +1,25 @@
+{nama : Rizki idham sofyan       }
+{NPM  :20.14.1.0020              }
+{nama project: tugas_prktikum.pas}
+program tugas_prakttikum;
+uses crt;
+
+var
+Nama: string;
+jeniskelamin :string;
+NPM: string;
+Alamat: string;
+TTL: string;
+NoHp: string;
+
+begin
+writeln('Contoh membaca dan menulis inputan');
+writeln('Masukkan Nama: '); readln(nama);
+writeln('Masukkan NPM: '); readln(npm);
+writeln('jenis kelamin :');readln(jeniskelamin);
+writeln('Masukkan Alamat: '); readln(alamat);
+writeln('Masukkan TTL: '); readln(ttl);
+writeln('No whatsapp: '); readln(nohp);
+
+
+end.
